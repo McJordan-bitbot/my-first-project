64483481cefe9533946b8e8d7427fb78d81ae4d3
McJordan <mcjordannanor20@gmail.com>
2026-04-22 01:51:27 +0000

    Clean up hello.py by removing unused code
    Removed unused functions and input prompts related to even number checking and weight conversion.

--- hello.py
+++ hello.py
@@ -1,14 +1,3 @@
-def is_even(n):
-        rem = n % 2
-        if rem == 0:
-            return True
-        else:
-            return False
-           
-n = int(input('n? '))
-print(is_even(n))
-
-
 #BILL SPLITTER
 running_total = 0
 
@@ -33,38 +22,3 @@ print('Bill per person:', final_bill)
 
 each_pays = round(final_bill, 2)
 print(f'Each person pays: {each_pays}')
-
-#TRY 2 (THIS ONE WORKS BETTER)
-weight = int(input("Weight: "))
-unit = input("(L)bs or (K)g: ") 
-if unit == "L" or unit == "l":
-    unit_in_kg = round(weight * 0.453592, 1)
-    print(f'Weight in Kg: {unit_in_kg}')
-elif unit == "K" or unit == "k":
-    unit_in_lbs = round(weight / 0.453592, 1)
-    print(f'Weight in Lbs: {unit_in_lbs}')
-else:
-    print("Invalid unit. Please enter 'L' for pounds or 'K' for kilograms.")
-
-#TRY 1
-weight = int(input("Weight: "))
-unit = input("(L)bs or (K)g: ") 
-num = float(2.20462)
-unit_in_kg = weight * float(num)
-if unit == "L" or unit == "l":
-    print("Weight in Kg: " + str(round(unit_in_kg, 1)))
-
-
-#cool one fam
-First = input("First number: ")
-Second = input("second number: ")
-sum = float(First) + float(Second)
-difference = float(First) - float(Second)
-division = float(First) / float(Second)
-multiplication = float(First) * float(Second)
-print('Hello, I am McJordan and I am becoming a software engineer! \nAnd my ARITHMETIC is given below')
-
-print(f"When you add, the Sum is: {str(sum)}")
-print(f"When you Subtract: {str(difference)}")
-print(f"When you Divide: {division}")
-print(f"When you Multiply: {str(multiplication)}")
